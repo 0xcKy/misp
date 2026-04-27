9a800d81f173692bdb4fc07803e64eed8485c7c3
Vini <222865488+0xcKy@users.noreply.github.com>
2026-04-27 15:56:16 -0300

    Update get_att.py
    Added a way to sort attributes into existing file without removing current lines/attributes

--- get_att.py
+++ get_att.py
@@ -9,8 +9,11 @@ response = misp.search(return_format='json', controller='attributes', eventid=id
 values = [attr.get("value") for attr in response.get("Attribute", [])] #get json from response and separete 'values'
 
 with open('ip_list_export.txt', 'a+') as f: #open file
-    for items in values: #write elements of list
-        f.write('%s\n' %items)
-    print("File written successfully")
-
-f.close() #closes file
+        f.seek(0)                                       #go to file beginning
+        exist = set(line.strip() for line in f)         #strip f content into 'exist' var
+        new = set(values) - exist                       #get 'values' content and remove 'exist' from it
+        f.seek(0,2)                                     #go to file end
+        for items in sorted(new):                       #write elements of list in sorted format
+                f.write('%s\n' %items)
+        print("File written successfully")
+f.close()
