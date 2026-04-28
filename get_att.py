bda57b1e4141fc9d2d8dddfcc0b132a36155d9ba
Vini <222865488+0xcKy@users.noreply.github.com>
2026-04-28 15:00:30 -0300

    Update get_att.py
    Added use of parser, list on pymisp attribute search/id/filename

--- get_att.py
+++ get_att.py
@@ -1,19 +1,34 @@
 from pymisp import PyMISP
-from pprint import pprint
+import argparse
+
+parser = argparse.ArgumentParser() #create parser
+parser.add_argument("-e", "--eventid", action="extend", nargs="+", type=str, help="list of events to download") #add list of events
+parser.add_argument("-a", "--attribute", nargs="+", help="type of attribute") #add attrib type
+parser.add_argument("-o", "--outfile", type=str, help="output file name") #add attrib type
+argument = parser.parse_args()
 
 MISP_URL = '<MISP_DOMAIN>'
 MISP_KEY = '<MISP_AUTH_KEY>'
-id = int(input("Event ID:\n"))
-misp = PyMISP(MISP_URL, MISP_KEY, ssl=False, debug=False) #create misp instance, uses URL and KEY, disable ssl and debug
-response = misp.search(return_format='json', controller='attributes', eventid=id, type_attribute=['ip-src','ip-dst'], to_ids=1) #uses search to return attributes using ID, type and IDS only
-values = [attr.get("value") for attr in response.get("Attribute", [])] #get json from response and separete 'values'
 
-with open('ip_list_export.txt', 'a+') as f: #open file
-        f.seek(0)                                       #go to file beginning
-        exist = set(line.strip() for line in f)         #strip f content into 'exist' var
-        new = set(values) - exist                       #get 'values' content and remove 'exist' from it
-        f.seek(0,2)                                     #go to file end
-        for items in sorted(new):                       #write elements of list in sorted format
-                f.write('%s\n' %items)
-        print("File written successfully")
-f.close()
+id = argument.eventid
+attrib = argument.attribute
+filename = argument.outfile
+
+#create misp instance, uses URL and KEY, disable ssl and debug
+misp = PyMISP(MISP_URL, MISP_KEY, ssl=False, debug=False)
+#uses search to return attributes using ID, type and IDS only
+response = misp.search(return_format='json', controller='attributes', eventid=id, exclude_decayed=True, type_attribute=attrib, to_ids=1)
+#get json from response and separete 'values'
+values = [attr.get("value") for attr in response.get("Attribute", [])]
+
+if __name__ == '__main__':
+
+        with open(str(filename), 'a+') as f:
+                f.seek(0)
+                exist = set(line.strip() for line in f)
+                new = set(values) - exist
+                f.seek(0,2)
+                for items in sorted(new):
+                        f.write('%s\n' %items)
+                print("File written successfully")
+        f.close()
