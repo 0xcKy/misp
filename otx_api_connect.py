d083cccd90edf1633d8bdc4ca58ebe0a7bcae411
Vini <222865488+0xcKy@users.noreply.github.com>
2026-05-07 16:43:38 -0300

    Update otx_api_connect.py
    Added get_all function, to get all subscribed pulses. Also added the argument to print the results on screen

--- otx_api_connect.py
+++ otx_api_connect.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python
+
 from OTXv2 import OTXv2
 from OTXv2 import IndicatorTypes
-import argparse
 from collections import defaultdict
+import argparse
 
 parser = argparse.ArgumentParser() #create parser
-parser.add_argument("-i", "--id", type=str, help="pulse id") #add pulse id argument
+exclusive = parser.add_mutually_exclusive_group()
+exclusive.add_argument("-p", "--pulse", type=str, help="pulse id") #add pulse id argument
+exclusive.add_argument("-a", "--all", action="store_true", help="download all subscribed pulses")
 parser.add_argument("-o", "--output", type=str, help="output file") #add output file argument
+parser.add_argument("-s", "--screen", action="store_true", help="show output on screen") #add output to screen
 argument = parser.parse_args()
+
 
 otx = OTXv2("<OTX_API_key>")
 ioc_result = defaultdict(list)
 filename = argument.output
-# Get all the indicators associated with a pulse
-pulses = otx.get_pulse_indicators(argument.id)
+result_list = []
 
 type_mapping = {
     'FileHash-MD5': 'md5',
@@ -27,13 +32,25 @@ type_mapping = {
     'SSLCertFingerprint': 'comment'
 }
 
+# Get all the indicators associated with a pulse
 def get_pulses():
+        pulses = otx.get_pulse_indicators(argument.pulse)
         for indicator in pulses:
                 ioc = indicator['indicator']
                 ioc_type = indicator['type']
                 ioc_type = type_mapping.get(ioc_type, ioc_type)
                 ioc_result[ioc_type].append(ioc)
-            
+# Get all the indicators associated with subscribed pulses
+def get_all():
+        pulses_json = otx.getall()
+        for pulses in pulses_json:
+                indicators = pulses['indicators']
+                for i in indicators:
+                        ioc = i['indicator']
+                        ioc_type = i['type']
+                        ioc_type = type_mapping.get(ioc_type, ioc_type)
+                        ioc_result[ioc_type].append(ioc)
+# Create or append indicators to output file
 def write_file(i):
         with open(str(filename), 'a+') as f:
                 f.seek(0)
@@ -42,13 +59,24 @@ def write_file(i):
                 f.seek(0,2)
                 for items in sorted(new):
                         f.write('%s\n' %items)
-
+def write_screen(i):
+        for line in i:
+                print(line)
 
 if __name__ == "__main__":
-        get_pulses()
+        if (argument.all):
+                get_all()
+        elif (argument.pulse):
+                get_pulses()
+
         if (filename):
-                result_list = []
                 for ioc_type, ioc in ioc_result.items():
                         for iocs in ioc:
                                 result_list.append(ioc_type+":"+iocs)
                 write_file(result_list)
+
+        if (argument.screen):
+                for ioc_type, ioc in ioc_result.items():
+                        for iocs in ioc:
+                                result_list.append(ioc_type+":"+iocs)
+                write_screen(result_list)
