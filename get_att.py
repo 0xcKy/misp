c0735dc47b35cdd648148f429f343b83cda12687
Vini <222865488+0xcKy@users.noreply.github.com>
2026-05-08 15:37:56 -0300

    Update get_att.py
    Organized code in functions

--- get_att.py
+++ get_att.py
@@ -11,26 +11,35 @@ argument = parser.parse_args()
 MISP_URL = '<MISP_DOMAIN>'
 MISP_KEY = '<MISP_AUTH_KEY>'
 
-id = argument.eventid
+id_number = argument.eventid
 attrib = argument.attribute
 filename = argument.outfile
 warning = argument.warning
+result_list = []
 
-#create misp instance, uses URL and KEY, disable ssl and debug
-misp = PyMISP(MISP_URL, MISP_KEY, ssl=False, debug=False)
-#uses search to return attributes using ID, type and IDS only
-response = misp.search(return_format='json', controller='attributes', eventid=id, exclude_decayed=True, type_attribute=attrib, to_ids=1, enforce_warninglist=warning)
-#get json from response and separete 'values'
-values = [attr.get("value") for attr in response.get("Attribute", [])]
+def get_att():
+        #create misp instance, uses URL and KEY, disable ssl and debug
+        misp = PyMISP(MISP_URL, MISP_KEY, ssl=False, debug=False)
+        #uses search to return attributes using ID, type and IDS only
+        response = misp.search(return_format='json', controller='attributes', eventid=id_number, exclude_decayed=True, type_attribute=attrib, to_ids=1, enforce_warninglist=warning)
+        #get json from response and separate 'values'
+        return [attr.get("value") for attr in response.get("Attribute", [])]
 
-if __name__ == '__main__':
-
+def write_file(i):
         with open(str(filename), 'a+') as f:
                 f.seek(0)
                 exist = set(line.strip() for line in f)
-                new = set(values) - exist
+                new = set(i) - exist
                 f.seek(0,2)
                 for items in sorted(new):
                         f.write('%s\n' %items)
                 print("File written successfully")
         f.close()
+
+if __name__ == '__main__':
+        if (id_number and attrib):
+                result_list = get_att()
+                if (filename):
+                        write_file(result_list)
+        else:
+                print("Event ID or attribute type missing. Use -h for help")
